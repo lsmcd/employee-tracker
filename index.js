6b71840939683e4cd39be42f00d50feb6f90e92f
Logan McDaniel <lsmcd@pm.me>
2024-03-19 05:26:53 -0600

    replace spaghetti code with promises

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ inquirer
             ]
         }
     ])
-    .then((answers) => {
+    .then(async (answers) => {
         switch (answers.action){
             case "View all departments":
                 viewAll(`department`);
@@ -44,56 +44,55 @@ inquirer
                 addTo("department", json);
                 break;
             case "Add a role":
-                var json = require("./db/json/prompts.json").addRole;
-                viewAll("department", json, listContent, addTo);
+                //var json = require("./db/json/prompts.json").addRole;
+                viewAll("department").then((result) => {
+                    var content = listContent(result, "Department");
+                    var json = require("./db/json/prompts.json").addRole;
+                    json[2].choices = content;
+                    addTo("department", json);
+                });
+                //viewAll("department", json, listContent, addTo);
                 break;
             case "Add an employee":
+                var role = listContent(await viewAll("role"), "Role");
+                var employee = listContent(await viewAll("employee"), "Employee");
                 var json = require("./db/json/prompts.json").addEmployee;
-                viewAll("employee", json, listContent, addTo, "role");
+                json[2].choices = role;
+                json[3].choices = employee;
+                addTo("department", json);
                 break;
             case "Update an employee role":
                 break;
         }
     })
     .catch((err) => console.error(err));
-viewAll = function(table, json, func, func2, tableOrResult){
-    db.query(`SELECT * FROM ${table}`, (err, result) => {
-        err && console.error(err);
-        if (arguments[1] && arguments[2] && arguments[3]){
-            if (typeof arguments[4] === "string"){
-                console.log("adding employees");
-                viewAll(tableOrResult, json, func, func2, result);
-            } 
-            else if (arguments[4]) {
-                console.log("adding employees 2")
-                func(result, json, func2, tableOrResult);
-            } else {
-                func(result, json, func2);
-            }
-        } else {
-            console.log(result);
-        }
-    });
+async function viewAll(table){
+    return await new Promise((resolve, reject) => {
+        db.query(`SELECT * FROM ${table}`, (err, result) => {
+            err && console.error(err);
+            resolve(result);
+        });
+    })
 }
-listContent = function(result, json, func, result2){
-    var names = "name";
-    if (arguments[3]){
+listContent = function(result, json){
+    var names = "name"
+    if (json === "Employee"){
         var content = [];
-        for (var i = 0; i < result2.length; i++){
-            content[i] = result2[i].first_name + " " + result2[i].last_name;
+        for (var i = 0; i < result.length; i++){
+            content[i] = result[i].first_name + " " + result[i].last_name;
         }
-        json[3].choices = content;
-        var names = "title";
+        return content;
     }
+    else if (json === "Role") {
+        names = "title"
+    }
+
     var content = [];
-    console.log(result);
     for (var i = 0; i < result.length; i++){
         content[i] = result[i][names];
     }
     console.log(content);
-    json[2].choices = content;
-    console.log(json);
-    func("idk", json);
+    return content;
 }
 
 addTo = function(table, prompt){
