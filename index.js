7c4872e59eac2e410987d8940acf11faae229556
Logan McDaniel <lsmcd@pm.me>
2024-03-18 21:59:13 -0600

    add add to database prompts WIP

--- index.js
+++ index.js
@@ -40,27 +40,68 @@ inquirer
                 viewAll("employee");
                 break;
             case "Add a department":
+                var json = require("./db/json/prompts.json").addDepartment;
+                addTo("department", json);
                 break;
             case "Add a role":
+                var json = require("./db/json/prompts.json").addRole;
+                viewAll("department", json, listContent, addTo);
                 break;
             case "Add an employee":
+                var json = require("./db/json/prompts.json").addEmployee;
+                viewAll("employee", json, listContent, addTo, "role");
                 break;
             case "Update an employee role":
                 break;
         }
     })
     .catch((err) => console.error(err));
-
-viewAll = function(table){
+viewAll = function(table, json, func, func2, tableOrResult){
     db.query(`SELECT * FROM ${table}`, (err, result) => {
         err && console.error(err);
-        console.log(result);
+        if (arguments[1] && arguments[2] && arguments[3]){
+            if (typeof arguments[4] === "string"){
+                console.log("adding employees");
+                viewAll(tableOrResult, json, func, func2, result);
+            } 
+            else if (arguments[4]) {
+                console.log("adding employees 2")
+                func(result, json, func2, tableOrResult);
+            } else {
+                func(result, json, func2);
+            }
+        } else {
+            console.log(result);
+        }
     });
 }
-addTo = function(table){
+listContent = function(result, json, func, result2){
+    var names = "name";
+    if (arguments[3]){
+        var content = [];
+        for (var i = 0; i < result2.length; i++){
+            content[i] = result2[i].first_name + " " + result2[i].last_name;
+        }
+        json[3].choices = content;
+        var names = "title";
+    }
+    var content = [];
+    console.log(result);
+    for (var i = 0; i < result.length; i++){
+        content[i] = result[i][names];
+    }
+    console.log(content);
+    json[2].choices = content;
+    console.log(json);
+    func("idk", json);
+}
+
+addTo = function(table, prompt){
+    console.log(prompt);
     inquirer
-        .prompt([
-        ])
+        .prompt(
+            prompt
+        )
         .then((answers) => {
         })
         .catch((err) => console.error(err));
